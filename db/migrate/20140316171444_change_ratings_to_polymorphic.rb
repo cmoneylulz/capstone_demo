807078134dcbbfaa6dab4038d87736d83043927b
Ashley Childress <achildr1@my.westga.edu>
2014-04-18 15:16:22 -0400

    fixed file causing heroku db migration to fail

--- db/migrate/20140316171444_change_ratings_to_polymorphic.rb
+++ db/migrate/20140316171444_change_ratings_to_polymorphic.rb
@@ -1,6 +1,5 @@
 class ChangeRatingsToPolymorphic < ActiveRecord::Migration
   def change
-		drop_table :comment_ratings
 		rename_table :interest_point_ratings, :ratings
 		remove_reference :ratings, :interest_point
 		add_reference :ratings, :ratable, polymorphic: true, index: true
